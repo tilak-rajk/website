82307cab4c2a9f054bb78a5f2cae3b062c4395c9
tilakmetacube <tilak.khandelwal@metacube.com>
2016-03-29 17:32:08 +0530

    Revert "Initial code commit that we received from BDC"
    This reverts commit 024d6846238900a8cb67c6aa667bbce4195c2025.

--- admin/instagram.config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $instagram = new Instagram(array(
-        'apiKey' => 'd35a2e066b2b4c38a7732451016171eb',
-        'apiSecret' => 'c98333ec27984235bb77b0a318550f35',
-        'apiCallback' => 'http://metacube.in/admin/success.php' // Callback URL
-    ));
-?>
--- include/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$global_dbname="Metacube";
-$global_username="Metacube";
-$global_password="Metacube@2016";
-mysql_connect("localhost", "$global_username", "$global_password");
-mysql_select_db("$global_dbname");
-?>
